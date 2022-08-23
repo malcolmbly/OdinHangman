504f35c5dde6a0bd3f315d88f16dc07acb95ea55
malcolmbly <malcolmhbailey@gmail.com>
2022-08-23 11:31:17 +0100

    Added YAML serialization to save/load game state

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,12 @@
 require 'yaml'
 
 class Hangman
+  @save_file_name = 'hangman.yaml'
+
+  def self.save_file_name
+    @save_file_name
+  end
+
   def initialize(dictionary_name)
     if load_game?
       load_game
@@ -8,15 +14,15 @@ class Hangman
       start_new_game(dictionary_name)
     end
     puts "Let's Play Hangman!"
+    
     play
   end
 
   def play
-    choose_word
     until game_over? || game_won?
       print_game
       puts 'Do you want to save? (type \'y\' if you want to)'
-      save_file if gets.chomp.downcase == 'y'
+      save_game if gets.chomp.downcase == 'y'
       guess = prompt_guess
       check_guess(guess)
     end
@@ -72,24 +78,21 @@ class Hangman
 
   def save_game
     game_state = { word: @word, guesses: @guesses, lives: @lives, revealed_word: @revealed_word }
-    file = File.open(@save_file, 'w+')
+    file = File.open(self.class.save_file_name, 'w+')
     YAML.dump(game_state, file)
   end
 
   def load_game?
     puts 'Would you like to load game, or start a new game?'
-    input = ''
-    until %w[L N Q].include?(input)
-      puts 'L for load, Q to close, anything else to start'
-      input = gets.chomp.upcase
-    end
-    exit if input == Q
+    puts 'L for load, Q to close, anything else to start'
+    input = gets.chomp.upcase
+    exit if input == 'Q'
     input == 'L'
   end
 
   def load_game
-    file = File.open(@save_file, 'r')
-    game_state = YAML.safe_load(file)
+    file = File.open(self.class.save_file_name, 'r')
+    game_state = YAML.load(file)
     @word = game_state[:word]
     @guesses = game_state[:guesses]
     @lives = game_state[:lives]
@@ -106,6 +109,7 @@ class Hangman
     end
     @guesses = []
     @lives = 6
+    choose_word
   end
 end
 
